083aae94630bf42d7e1fe9e3bf4d7fe631801dcd
zarzouram <muhammadzarzoura@gmail.com>
2021-01-22 11:29:29 +0100

    update hyperparameters__init__

--- hotam/default_hyperparamaters/__init__.py
+++ hotam/default_hyperparamaters/__init__.py
@@ -1,74 +1,89 @@
-
 # from hotam.default_hyperparamaters.lstm_crf import lstm_crf_hps
 # from hotam.default_hyperparamaters.lstm_crf import lstm_cnn_crf_hps
 # from hotam.default_hyperparamaters.lstm_crf import joint_pointer_nn_hps
 # from hotam.default_hyperparamaters.lstm_crf import lstm_dist_hps
 
-
-
-lstm_crf_hps =  {
-                    "optimizer": "sgd",
-                    "lr": 0.001,
-                    "hidden_dim": 256,
-                    "num_layers": 2,
-                    "bidir": True,
-                    "fine_tune_embs": False,
-                    "batch_size": 32,
-                    "max_epochs":150,
-                    }
-            
+lstm_crf_hps = {
+    "optimizer": "sgd",
+    "lr": 0.001,
+    "hidden_dim": 256,
+    "num_layers": 2,
+    "bidir": True,
+    "fine_tune_embs": False,
+    "batch_size": 32,
+    "max_epochs": 150,
+}
 
 lstm_cnn_crf_hps = {
-                    "optimizer": "sgd",
-                    "lr": 0.001,
-                    "hidden_dim": 256,
-                    "char_dim": 100,
-                    "kernel_size": 3,
-                    "num_layers": 2,
-                    "bidir": True,
-                    "batch_size": 32,
-                    "max_epochs":100,
-                    }
-
+    "optimizer": "sgd",
+    "lr": 0.001,
+    "hidden_dim": 256,
+    "char_dim": 100,
+    "kernel_size": 3,
+    "num_layers": 2,
+    "bidir": True,
+    "batch_size": 32,
+    "max_epochs": 100,
+}
 
 joint_pointer_nn_hps = {
-                        "optimizer": "adam",
-                        "lr": 0.001,
-                        "encoder_input_dim": 256,
-                        "encoder_hidden_dim": 256,
-                        "encoder_num_layers":2,
-                        "encoder_bidir":True,
-                        "decoder_hidden_dim": 512,
-                        "feature_dropout": 0.8,
-                        "encoder_dropout": 0.8,
-                        "decoder_dropout": 0.8,
-                        "task_weight":0.5,
-                        "batch_size": 16,
-                        "max_epochs":1000,
-                        }
+    "optimizer": "adam",
+    "lr": 0.001,
+    "encoder_input_dim": 256,
+    "encoder_hidden_dim": 256,
+    "encoder_num_layers": 2,
+    "encoder_bidir": True,
+    "decoder_hidden_dim": 512,
+    "feature_dropout": 0.8,
+    "encoder_dropout": 0.8,
+    "decoder_dropout": 0.8,
+    "task_weight": 0.5,
+    "batch_size": 16,
+    "max_epochs": 1000,
+}
 
 lstm_dist_hps = {
-                "optimizer": "sgd",
-                "lr": 0.001,
-                "hidden_dim": 256,
-                "num_layers": 1,
-                "bidir": True,
-                "batch_size": 10,
-                "max_epochs":10,
-                "alpha": 0.5,
-                "beta": 0.5
-                }
+    "optimizer": "sgd",
+    "lr": 0.001,
+    "hidden_dim": 256,
+    "num_layers": 1,
+    "bidir": True,
+    "batch_size": 10,
+    "max_epochs": 10,
+    "alpha": 0.5,
+    "beta": 0.5
+}
+
+lstm_er = {
+    # "token_embedding_size" = 256,  # Embed dimension for tokens
+    "label_embedding_size": 20,  # Embed dimension for entity label
+    "dep_embedding_size": 50,  # Embed dimension for depedency label
+    "seq_lstm_h_size": 128,  # Sequential LSTM hidden size
+    "tree_lstm_h_size": 128,  # Tree LSTM hidden size
+    "ner_hidden_size": 45,  # Entity recognition layer hidden size
+    "ner_output_size": 30,  # Entity recognition layer output size
+    "re_hidden_size": 30,  # Relation extraction layer hidden size
+    "re_output_size": 30,  # Relation extraction layer output size
+    "seq_lstm_num_layers": 1,  # Sequential LSTM number of layer
+    "lstm_bidirectional": True,  # Sequential LSTM bidirection
+    "tree_bidirectional": True,  # Tree LSTM bidirection
+    "dropout": 0,
+    "optimizer": "adam",
+    "lr": 0.001,
+    "max_epochs": 10,
+    "batch_size": 10,
+}
 
 
 def get_default_hps(model_name):
-    
+
     if model_name.lower() == "lstm_crf":
         return lstm_crf_hps
     elif model_name.lower() == "lstm_cnn_crf":
         return lstm_cnn_crf_hps
     elif model_name.lower() == "jointpn":
         return joint_pointer_nn_hps
-    elif model_name.lower()== "lstm_dist":
+    elif model_name.lower() == "lstm_dist":
         return lstm_dist_hps
     else:
         raise NotImplementedError
