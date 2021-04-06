4506a60ef73b9dccbbce4f97250cef3fadcb7e85
Axel <axelalmquist@gmail.com>
2021-04-06 18:00:28 +0200

    bug in dataset_preprocessor.py

--- start.py
+++ start.py
@@ -1,29 +1,28 @@
 from hotam.datasets import PE
 from hotam import Pipeline
 from hotam.features import DummyFeature, OneHots
-from hotam.nn.models import LSTM_ER
+from hotam.nn.models import LSTM_CRF
 from hotam.nn.default_hyperparamaters import get_default_hps
 
 
 exp = Pipeline(
-    project="debugging",
-    dataset=PE(
-                tasks=["seg+label", "link", "link_label"],
-                prediction_level="token",
-                sample_level="document",
-                ),
-    encodings=["pos", "deprel", "dephead"],
-    features=[
-        DummyFeature(),
-        OneHots("pos"),
-        OneHots("deprel")
-    ])
+                project="debugging",
+                dataset=PE(
+                            tasks=["seg+label"],
+                            prediction_level="token",
+                            sample_level="sentence",
+                            ),
+                #encodings=["pos", "deprel", "dephead"],
+                features=[
+                            DummyFeature(),
+                                ]
+            )
 
 exp.fit(
-    model=LSTM_ER,
-    hyperparamaters=get_default_hps(LSTM_ER.name()),
-    # exp_logger=exp_logger,
-    # ptl_trn_args=dict(
-    #                     gpus=[2]
-    #                     )
-)
+        model=LSTM_CRF,
+        hyperparamaters=get_default_hps(LSTM_CRF.name()),
+        # exp_logger=exp_logger,
+        # ptl_trn_args=dict(
+        #                     gpus=[2]
+        #                     )
+        )
